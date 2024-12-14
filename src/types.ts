ba40cd7d9316fe841c0c4f92bdce594424d72474
DamirPavlik <damir.pavlik@gmail.com>
2024-12-14 21:33:08 +0100

    feat: prop patch type

--- src/types.ts
+++ src/types.ts
@@ -3,3 +3,12 @@ type vNode = | string | {
     props?: { [key: string]: string | ((event: Event) => void) }; 
     children?: vNode[]; 
 };
+
+type PropPatch = | {
+    type: "SET_PROP",
+    key: string,
+    value: string | ((event: Event) => void)
+} | {
+    type: "REMOVE_PROP",
+    key: string,
+}
