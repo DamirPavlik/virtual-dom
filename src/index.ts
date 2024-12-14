6996ea4495087199322dfd3478e650d3a00292e7
DamirPavlik <damir.pavlik@gmail.com>
2024-12-14 21:13:31 +0100

    feat: create element fn

--- src/index.ts
+++ src/index.ts
@@ -17,3 +17,26 @@ const vNode: vNode = {
     ]
 }
 
+function createElement(vNode: vNode): Node {
+    if (typeof vNode === "string") {
+        return document.createTextNode(vNode);
+    }
+
+    let element = document.createElement(vNode.type);
+
+    if (vNode.props) {
+        for(const [k, v] of Object.entries(vNode.props)) {
+            if (typeof v === "function") {
+                element.addEventListener(k, v as EventListener);
+            } else {
+                element.setAttribute(k, v);
+            }
+        }
+    }
+
+    (vNode.children || []).forEach(child => {
+        element.appendChild(createElement(child));
+    })
+
+    return element;
+}
