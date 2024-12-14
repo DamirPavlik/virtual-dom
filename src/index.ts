82f5716e809167840a2b4cf93c426d81dee096a0
DamirPavlik <damir.pavlik@gmail.com>
2024-12-14 21:00:58 +0100

    feat: init setup

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+const vNode: vNode = {
+    type: "div",
+    props: {
+        "id": "something"
+    },
+    children: [
+        {
+            type: "h1",
+            props: {},
+            children: ["Hello World"]
+        },
+        {
+            type: "p",
+            props: {},
+            children: ["Virtual DOM"]
+        },
+    ]
+}
+
